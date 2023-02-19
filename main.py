5103845e19016f01300e9132bf52a4307668ce35
Toms Dejus <Toms.Dejus@edu.rtu.lv>
2023-02-19 23:14:52 +0200

    Updated I for input

--- main.py
+++ main.py
@@ -38,7 +38,10 @@ def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
-    print(mismatch)
+    if text[0] == "I":
+        text = input()
+        mismatch = find_mismatch(text)
+        print(mismatch)
 
 if __name__ == "__main__":
     main()
