c61a7140cfbde66573dcc0dbabc061dedf76bf9a
Toms Dejus <Toms.Dejus@edu.rtu.lv>
2023-02-19 23:01:36 +0200

    Completed update

--- main.py
+++ main.py
@@ -12,20 +12,33 @@ def are_matching(left, right):
 def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
+        base1 = i+1
         if next in "([{":
             # Process opening bracket, write your code here
-            pass
+            opening_brackets_stack.append(base1)
+            opening_brackets_stack.append(next)
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            pass
+            if len(opening_brackets_stack) > 0:
+                if not are_matching(opening_brackets_stack[-1], next):
+                    return base1
+                else:
+                    opening_brackets_stack.pop()
+                    opening_brackets_stack.pop()
+            else:
+                return base1
 
+    if len(opening_brackets_stack) > 0:
+        return opening_brackets_stack[0]
+
+    return "Success"
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
-
+    print(mismatch)
 
 if __name__ == "__main__":
     main()
